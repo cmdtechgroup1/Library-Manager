8f039962878d378c94b096d611991550a72aa05a
solidtm <richardebboh@gmail.com>
2021-03-22 09:20:34 +0100

    test commit

--- src/com/cmd/ui/main/Controller.java
+++ src/com/cmd/ui/main/Controller.java
@@ -1,4 +1,8 @@
 package com.cmd.ui.main;
 
 public class Controller {
+    public static void main(String[] args) {
+        System.out.println("This is just for test" +
+                "");
+    }
 }
