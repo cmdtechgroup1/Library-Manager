ce99ec3415c078970c7fa53a83ff504b8a349938
Michael Ikechukwu <michaelikechukwu2000@gmail.com>
2021-04-28 17:16:57 +0100

    local commit

--- src/com/cmd/ui/addstudent/AddStudentController.java
+++ src/com/cmd/ui/addstudent/AddStudentController.java
@@ -1,4 +1,35 @@
 package com.cmd.ui.addstudent;
 
+import com.cmd.ui.util.alert.AlertMaker;
+import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+
+import java.util.Arrays;
+
 public class AddStudentController {
+
+    @FXML
+    private StackPane rootPane;
+
+    @FXML
+    private HBox hBox;
+
+    @FXML
+    private Label closeLabel, minimizeLabel;
+
+
+
+
+    //    Method implementation abstracted using the RequestHandler Singleton class
+    @FXML
+    public void closeLabelPressed() {
+        JFXButton yesButton = new JFXButton("Okay");
+        JFXButton cancelButton = new JFXButton("Cancel");
+        AlertMaker.showMaterialDialog(rootPane, hBox, Arrays.asList(yesButton, cancelButton), "Exit", "Are you sure you want to exit?");
+        yesButton.setOnAction(event -> Platform.exit());
+    }
 }
